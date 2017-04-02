e05e38179ebe7eb264e55b9758a29fe47e50fe92
Michael Liu <mike666234@gmail.com>
2017-04-02 04:29:31 -0700

    Mongoose save works

--- app.js
+++ app.js
@@ -1,12 +1,65 @@
 var express = require("express");
 var app= express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+// Mongoose
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test");
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("connected to mongodb");
+});
+
+// Mongoose Schema for busser
+var Schema = mongoose.Schema;
+var busserSchema = new Schema({
+  email: String,
+  recency: Number,
+  likelihood: Number
+});
+var Busser = mongoose.model("Busser", busserSchema);
+
+// Controllers
 var dataController = require("./dataController");
 
+// HTTP Methods
 app.get("/", function(req, res){
   res.end("Hello World");
 });
 
 app.get("/data", dataController.getData);
+
+app.post("/data", function(req, res){
+  
+  
+  var newEmail = req.body.busser.email;
+  var newRecency = req.body.busser.recency;
+  var newLikelihood = req.body.busser.likelihood;
+  
+  var newBusser = new Busser({
+    email: newEmail,
+    recency: newRecency,
+    likelihood: newLikelihood
+  });
+  
+  newBusser.save(function (err) {
+    if (err) return handleError(err);
+    
+    // saved!
+    console.log("saved new busser: " + newEmail);
+    // var bussers;
+    Busser.find(function (err, bussers) {
+      if (err) return console.error(err);
+      res.send(bussers);
+    });
+  })
+});
+// app.patch("/data", dataController.patchData);
+// app.delete("/data", dataController.deleteData);
 
 app.listen(3000);
 console.log("running...");
--- dataController.js
+++ dataController.js
@@ -3,33 +3,51 @@ exports.getData = function(req, res){
     data: [
       {
         "email": "1@lol.com",
-        "transport": "self"
+        "recency": 1,
+        "likelihood": 5
       },
       {
         "email": "2@lol.com",
-        "transport": "provide",
-        "slots": 5,
-        "confirmed": "false"
+        "recency": 2,
+        "likelihood": 0
       },
       {
         "email": "3@lol.com",
-        "transport": "need",
-        "transportOptions": [
-          "bus",
-          "car"
-        ],
-        "confirmed": "false"
+        "recency": 1,
+        "likelihood": 3
       },
       {
         "email": "4@lol.com",
-        "transport": "need",
-        "transportOptions": [
-          "car"
-        ],
-        "confirmed": "false"
+        "recency": 3,
+        "likelihood": 2
       }
     ]
   };
 
   res.send(list);
+};
+
+exports.postData = function(req, res){
+  
+  var newEmail = req.body.email;
+  var newRecency = req.body.recency;
+  var newLikelihood = req.body.likelihood;
+  
+  var newBusser = new Busser({
+    email: newEmail,
+    recency: newRecency,
+    likelihood: newLikelihood
+  });
+  
+  newBusser.save(function (err) {
+    if (err) return handleError(err);
+    
+    // saved!
+    console.log("saved new busser: " + newEmail);
+    // var bussers;
+    Busser.find(function (err, bussers) {
+      if (err) return console.error(err);
+      res.send(bussers);
+    });
+  })
 };
